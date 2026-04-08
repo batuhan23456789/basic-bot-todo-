0fd527711e4c8aa9e506f5b926e0c6de71f3ee6c
Batuhan Bulut <batbul0702@gmail.com>
2026-04-08 18:41:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,48 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} olarak giriş yaptık')
 
+
 @bot.command()
 async def hello(ctx):
     await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+
+@bot.command()
+async def help(ctx):
+    await ctx.send(f'')
+
+@bot.command()
+async def cıkart(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left - right)
+
+@bot.command()
+async def bol(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def carp(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left * right)
+
 
 @bot.command()
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 
+
 @bot.command()
 async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
 
 @bot.command()
 async def roll(ctx, dice: str):
@@ -31,14 +55,17 @@ async def roll(ctx, dice: str):
         rolls, limit = map(int, dice.split('d'))
     except Exception:
         await ctx.send('Format has to be in NdN!')
+
         return
 
     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
     await ctx.send(result)
+
 
 @bot.command()
 async def add(ctx, left: int, right: int):
     """Adds two numbers together."""
     await ctx.send(left + right)
 
-bot.run("❗TOKEN RIGHT HERE❗")
+
+bot.run("")
